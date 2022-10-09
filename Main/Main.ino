b21b8393c6a822c044836d43d4ec75717d1b046d
Mar24xd <mateo.ramirez01@usa.edu.co>
2022-10-09 12:33:03 -0500

    Nueva carpeta main

---
+++ Main/Main.ino
@@ -0,0 +1,22 @@
+#include "sensado.h"
+
+
+#define LED 2
+#define BT 15
+void setup(){
+  pinMode(LED,OUTPUT);
+  pinMode(BT,INPUT)
+  Serial.begin(115200);
+}
+void loop(){
+  int lec=digitalRead(BT);
+  if(lec==HIGH){
+
+  }
+
+  if (bufferSent) {
+    // Do something if values were sent properly
+    digitalWrite(LED,HIGH)
+    Serial.println("Values sent by the device");
+  }
+}
